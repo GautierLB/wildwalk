c429cb9520150bac982a043d6a1b280244d22337
mickael.sinardet@gmail.com <mickael.sinardet@gmail.com@6f5db719-8c7e-5101-9e28-6e47ecc2f14e>
2014-04-14 13:13:24 +0000

    == View ==
    #Fix
    - textview (pour afficher la location)
    - btnStart
    
    #New
    - ajout de spinner pour le mode de randonnée(combo box)
    - ajout timer (Pour les randonnées)
    - fonction btnStart

--- trunk/src/com/example/wildwalk/StartActivity.java
+++ trunk/src/com/example/wildwalk/StartActivity.java
@@ -1,32 +1,44 @@
 package com.example.wildwalk;
 
-
-import java.util.List;
-
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView.FindListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class StartActivity extends Fragment{
+public class StartActivity extends Fragment {
+	private Button btnStart;
+	private Context context;
+	private Spinner spinner1;
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		context= this.getActivity();
+		
+		View start = inflater.inflate(R.layout.activity_start, container, false);
+		((TextView) start.findViewById(R.id.textView)).setText("Gautier Lebissonnais");
+		btnStart = (Button) start.findViewById(R.id.btnStart);
+		
+		btnStart.setOnClickListener(new View.OnClickListener() {
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View view) {
 
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {		 
-			View start = inflater.inflate(R.layout.activity_start, container, false);
-		    ((TextView)start.findViewById(R.id.textView)).setText("START");
-			return start;
-		    
+				Toast toast = Toast.makeText(context, "START", Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		});
+
+		return start;
+
 	}
 
-	
 }
-
 
 
 
