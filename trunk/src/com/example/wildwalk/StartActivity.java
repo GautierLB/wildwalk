7a498684d955b4901a469fb793d218b07935a476
gautier.lb@gmail.com <gautier.lb@gmail.com@6f5db719-8c7e-5101-9e28-6e47ecc2f14e>
2014-04-15 12:25:55 +0000

    == Model == #new - Ajout de la classe LocationData - Ajout de l'interface LocationDataInterface
    == View ==
    
    #new
    - Quand on clique sur start, affiche la location.
    
    == Manifest ==
    
    #fix 
    - Ajout des différentes eprmissions requises

--- trunk/src/com/example/wildwalk/StartActivity.java
+++ trunk/src/com/example/wildwalk/StartActivity.java
@@ -1,5 +1,7 @@
 package com.example.wildwalk;
 
+import com.google.android.gms.common.ConnectionResult;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
@@ -12,17 +14,21 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class StartActivity extends Fragment {
+public class StartActivity extends Fragment implements LocationDataInterface {
 	private Button btnStart;
 	private Context context;
 	private Spinner spinner1;
+	private TextView location;
+	LocationData loc;
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		context= this.getActivity();
 		
 		View start = inflater.inflate(R.layout.activity_start, container, false);
-		((TextView) start.findViewById(R.id.textView)).setText("Gautier Lebissonnais");
+		location =((TextView) start.findViewById(R.id.textView));
 		btnStart = (Button) start.findViewById(R.id.btnStart);
+		loc = new LocationData(this, context);
+		loc.connect();
 		
 		btnStart.setOnClickListener(new View.OnClickListener() {
 			@SuppressLint("NewApi")
@@ -31,12 +37,31 @@ public class StartActivity extends Fragment {
 
 				Toast toast = Toast.makeText(context, "START", Toast.LENGTH_SHORT);
 				toast.show();
+				this.getLocation();
+			}
+
+			private void getLocation() {
+				location.setText(loc.getLocation());				
 			}
 		});
 
 		return start;
 
 	}
+
+	@Override
+	public void disconnected() {
+		Toast.makeText(this.context,"Location client disconnected",Toast.LENGTH_SHORT).show();	
+	}
+
+	@Override
+	public void connectionFailed(ConnectionResult co) {
+		Toast.makeText(this.context, "Connection Failure : " + 
+			      co.getErrorCode(),
+			      Toast.LENGTH_SHORT).show();
+		
+	}
+	
 
 }
 
